6f07c77110a1828a85d3de1c8d84f36121915155
Andrea Canedoli <andrea@hibase.co>
2023-12-17 15:22:53 +0100

    adding cors headers for testing

--- src/server.js
+++ src/server.js
@@ -14,6 +14,15 @@ const databaseConfig = {
   database: process.env.DB_NAME || 'your_database_name',
 };
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // Create a MySQL connection pool
 const pool = mysql.createPool(databaseConfig);
 
