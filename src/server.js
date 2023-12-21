ff55d3b669bfb6c53c2f282e5fe4f7dc6f8ed2ce
Andrea Canedoli <andrea@hibase.co>
2023-12-21 17:27:03 +0100

    fixing cors

--- src/server.js
+++ src/server.js
@@ -23,9 +23,9 @@ if(Object.values(databaseConfig).find(v => !v)) {
 
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
