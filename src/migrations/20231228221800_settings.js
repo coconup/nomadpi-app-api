bae22ae4482b792c4a12a5853cdbe06376208252
Andrea Canedoli <andrea@hibase.co>
2023-12-28 22:59:04 +0100

    `settings` table

--- src/migrations/20231228221800_settings.js
+++ src/migrations/20231228221800_settings.js
@@ -9,7 +9,7 @@ exports.up = function(knex) {
     table.string('value');
   })
   .then(() => {
-    knex('settings').insert([
+    return knex('settings').insert([
       {
         setting_key: 'gpsd_usb_device',
         label: 'GPS device'
