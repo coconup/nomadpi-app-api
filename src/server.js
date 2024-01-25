09cb08f7e6c7157f53c8af7f178806e62faa8f34
Andrea Canedoli <andrea@hibase.co>
2024-01-25 11:11:21 +0100

    moving toggle switch logic to api

--- src/server.js
+++ src/server.js
@@ -229,6 +229,7 @@ knexInstance.migrate.latest().then(() => {
     }
 
     const switchItem = await getSwitchItem(switchType, switchId, res);
+    console.error('switchItem', switchItem);
     
     if(!switchItem) {
       return res.status(404).json({ error: `${switchType} not found` });
@@ -250,6 +251,8 @@ knexInstance.migrate.latest().then(() => {
         }
       });
     };
+
+    console.error('payload', payload);
 
     req.body = payload;
     forwardRequest(req, res, vanPiApiRootUrl, '/relays/state');
